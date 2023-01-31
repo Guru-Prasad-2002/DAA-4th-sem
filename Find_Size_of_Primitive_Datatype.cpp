fd78a9796bc52dbd8cf277a51b3ac81edd91820a
Guru <gurujissrr@gmail.com>
2023-01-31 23:59:59 +0530

    Removed unwanted files

--- Find_Size_of_Primitive_Datatype.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    cout<<"SPACE FOT INT="<<sizeof(int)<<endl;
-    cout<<"SPACE FOT FLOAT="<<sizeof(float)<<endl;
-    cout<<"SPACE FOT DOUBLE="<<sizeof(double)<<endl;
-    cout<<"SPACE FOT CHAR="<<sizeof(char)<<endl;
-}
